b19d123dc31a261b783af67d8084f031b2b1d636
jursetti <jerry.ursetti@anthem.com>
2018-09-19 14:53:29 -0700

    Refactor vertx usage in Start method.

--- src/main/java/com/antm/fdsm/web/webserver/Start.java
+++ src/main/java/com/antm/fdsm/web/webserver/Start.java
@@ -1,10 +1,11 @@
 package com.antm.fdsm.web.webserver;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+//import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.net.JksOptions;
-import io.vertx.core.net.NetServerOptions;
+//import io.vertx.core.net.NetServerOptions;
 
 public class Start extends AbstractVerticle {
 
@@ -17,6 +18,25 @@ public class Start extends AbstractVerticle {
 		}
 	}
 	
+    public void start() throws Exception {
+
+        Vertx vertx = Vertx.vertx();
+        
+		System.out.println("Get jks file");
+        JksOptions jks = new JksOptions();
+        jks.setPath("/home/opc/fdsm-compute-dev-adm-web.anthem.com.jks");
+        jks.setPassword("Antm2017#");
+
+        System.out.println("Create HTTP Server");
+        vertx.createHttpServer(new HttpServerOptions().setSsl(true).setKeyStoreOptions(jks))
+        	.requestHandler(r -> {
+    	  		r.response().end("<h1>Hello from my first Vert.x application</h1>");
+        	})
+        	.listen(443);
+        
+    }
+        
+	/*
 	public void start(Future<Void> fut) {
 		
 		System.out.println("Get jks file");
@@ -24,14 +44,13 @@ public class Start extends AbstractVerticle {
         jks.setPath("/home/opc/fdsm-compute-dev-adm-web.anthem.com.jks");
         jks.setPassword("Antm2017#");
 
-        //NetServerOptions nso = new NetServerOptions()
-        //     .setSsl(true).setKeyStoreOptions(jks);
-        
         System.out.println("Create HTTP Server");
         vertx.createHttpServer(new HttpServerOptions().setSsl(true).setKeyStoreOptions(jks))
         	.requestHandler(r -> {
     	  		r.response().end("<h1>Hello from my first Vert.x application</h1>");
         	})
+        	.listen(443);
+      */  /*
     	  	.listen(443, result -> {
     	  		if ( result.succeeded() ) {
     	  			System.out.println("Success!");
@@ -42,6 +61,7 @@ public class Start extends AbstractVerticle {
     	  			fut.fail(result.cause());
     	  		}
     	  	});
+    	  	*/
         /*
 	  vertx
 	  	.createHttpServer().requestHandler(r -> {
@@ -57,6 +77,6 @@ public class Start extends AbstractVerticle {
 	  	});
 	  	*/
 
-  }
+  //}
 }
 
