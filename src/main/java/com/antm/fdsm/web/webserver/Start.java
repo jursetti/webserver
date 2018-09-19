690c69f238bd3e31b499de2c42b4b964daa72add
jursetti <jerry.ursetti@anthem.com>
2018-09-19 10:38:20 -0700

    Add ssl.

--- src/main/java/com/antm/fdsm/web/webserver/Start.java
+++ src/main/java/com/antm/fdsm/web/webserver/Start.java
@@ -2,6 +2,9 @@ package com.antm.fdsm.web.webserver;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.JksOptions;
+import io.vertx.core.net.NetServerOptions;
 
 public class Start extends AbstractVerticle {
 
@@ -16,6 +19,26 @@ public class Start extends AbstractVerticle {
 	
 	public void start(Future<Void> fut) {
 	  
+        JksOptions jks = new JksOptions();
+        jks.setPath("/home/opc/fdsm-compute-dev-adm-web.anthem.com.jks");
+        jks.setPassword("Antm2017#");
+
+        //NetServerOptions nso = new NetServerOptions()
+        //     .setSsl(true).setKeyStoreOptions(jks);
+        
+        vertx.createHttpServer(new HttpServerOptions().setSsl(true).setKeyStoreOptions(jks))
+        	.requestHandler(r -> {
+    	  		r.response().end("<h1>Hello from my first Vert.x application</h1>");
+        	})
+    	  	.listen(443, result -> {
+    	  		if ( result.succeeded() ) {
+    	  			fut.complete();
+    	  		}
+    	  		else {
+    	  			fut.fail(result.cause());
+    	  		}
+    	  	});
+        /*
 	  vertx
 	  	.createHttpServer().requestHandler(r -> {
 	  		r.response().end("<h1>Hello from my first Vert.x application</h1>");
@@ -28,6 +51,7 @@ public class Start extends AbstractVerticle {
 	  			fut.fail(result.cause());
 	  		}
 	  	});
+	  	*/
 
   }
 }
