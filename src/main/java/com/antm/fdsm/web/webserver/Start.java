97e6f0d1231b7c2f75a62e1502d1cd6f19b020c6
jursetti <jerry.ursetti@anthem.com>
2018-09-19 12:01:04 -0700

    Add print statements to track progress.

--- src/main/java/com/antm/fdsm/web/webserver/Start.java
+++ src/main/java/com/antm/fdsm/web/webserver/Start.java
@@ -18,7 +18,8 @@ public class Start extends AbstractVerticle {
 	}
 	
 	public void start(Future<Void> fut) {
-	  
+		
+		System.out.println("Get jks file");
         JksOptions jks = new JksOptions();
         jks.setPath("/home/opc/fdsm-compute-dev-adm-web.anthem.com.jks");
         jks.setPassword("Antm2017#");
@@ -26,15 +27,18 @@ public class Start extends AbstractVerticle {
         //NetServerOptions nso = new NetServerOptions()
         //     .setSsl(true).setKeyStoreOptions(jks);
         
+        System.out.println("Create HTTP Server");
         vertx.createHttpServer(new HttpServerOptions().setSsl(true).setKeyStoreOptions(jks))
         	.requestHandler(r -> {
     	  		r.response().end("<h1>Hello from my first Vert.x application</h1>");
         	})
     	  	.listen(443, result -> {
     	  		if ( result.succeeded() ) {
+    	  			System.out.println("Success!");
     	  			fut.complete();
     	  		}
     	  		else {
+    	  			System.out.println("Failure!");
     	  			fut.fail(result.cause());
     	  		}
     	  	});
