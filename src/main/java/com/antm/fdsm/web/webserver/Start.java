9ad4b0eb8d724f7935cd2ef2b1255ddc7d9d4c65
jursetti <jerry.ursetti@anthem.com>
2018-09-27 11:02:06 -0700

    Change messaging to tinylog.

--- src/main/java/com/antm/fdsm/web/webserver/Start.java
+++ src/main/java/com/antm/fdsm/web/webserver/Start.java
@@ -1,14 +1,13 @@
 package com.antm.fdsm.web.webserver;
 
+import com.antm.fdsm.web.webserver.Def;
+import org.pmw.tinylog.Logger;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Handler;
-//import io.vertx.core.Future;
-//import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.net.JksOptions;
-//import io.vertx.core.net.NetServerOptions;
 
 public class Start extends AbstractVerticle {
 
@@ -20,18 +19,19 @@ public class Start extends AbstractVerticle {
     @Override
     public void start() throws Exception {
     	
-		System.out.println("Get jks file");
+    	System.out.println(Def.SLINE);
+		Logger.info("Get jks file");
         JksOptions jks = new JksOptions();
         jks.setPath("/home/opc/certs/keystore.jks");
         jks.setPassword("Antm2017#");
         
-        System.out.println("Create HTTP Server");
+        Logger.info("Create HTTP Server");
         httpServer = vertx.createHttpServer(new HttpServerOptions().setSsl(true).setKeyStoreOptions(jks).setLogActivity(true));
 
-        System.out.println("Create request handler");
+        Logger.info("Create request handler");
         httpServer.requestHandler(new Handler<HttpServerRequest>() {
             public void handle(HttpServerRequest request) {
-                System.out.println("Launch page " + request.path());
+            	Logger.info("Launch page " + request.path());
                 if (request.path().equals("/")) {
                 	request.response().sendFile(WEBROOT + "index.html");
                 }
@@ -49,88 +49,15 @@ public class Start extends AbstractVerticle {
             }
         });
 
-        System.out.println("Start HTTP server on port " + PORT);
+        Logger.info("Start HTTP server on port " + PORT);
         httpServer.listen(PORT, res -> {
             if (res.succeeded()) {
-                System.out.println("HTTP server is ready!");
+            	Logger.info("HTTP server is ready!");
               } else {
-                System.out.println("HTTP server failed!");
+            	  Logger.info("HTTP server failed!");
               }
             });
     }
     
-	/*
-	public void main() {
-		try {
-			start();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	*/
-	
-    /*
-    public void start() throws Exception {
-
-        Vertx vertx = Vertx.vertx();
-        
-		System.out.println("Get jks file");
-        JksOptions jks = new JksOptions();
-        jks.setPath("/home/opc/certs/fdsm-compute-dev-adm-web.anthem.com.jks");
-        jks.setPassword("Antm2017#");
-
-        System.out.println("Create HTTP Server");
-        vertx.createHttpServer(new HttpServerOptions().setSsl(true).setKeyStoreOptions(jks))
-        	.requestHandler(r -> {
-    	  		r.response().end("<h1>Hello from my first Vert.x application</h1>");
-        	})
-        	.listen(PORT);
-        System.out.println("HTTP Server running on port " + PORT);
-    }
-    */
-        
-	/*
-	public void start(Future<Void> fut) {
-		
-		System.out.println("Get jks file");
-        JksOptions jks = new JksOptions();
-        jks.setPath("/home/opc/fdsm-compute-dev-adm-web.anthem.com.jks");
-        jks.setPassword("Antm2017#");
-
-        System.out.println("Create HTTP Server");
-        vertx.createHttpServer(new HttpServerOptions().setSsl(true).setKeyStoreOptions(jks))
-        	.requestHandler(r -> {
-    	  		r.response().end("<h1>Hello from my first Vert.x application</h1>");
-        	})
-        	.listen(443);
-      */  /*
-    	  	.listen(443, result -> {
-    	  		if ( result.succeeded() ) {
-    	  			System.out.println("Success!");
-    	  			fut.complete();
-    	  		}
-    	  		else {
-    	  			System.out.println("Failure!");
-    	  			fut.fail(result.cause());
-    	  		}
-    	  	});
-    	  	*/
-        /*
-	  vertx
-	  	.createHttpServer().requestHandler(r -> {
-	  		r.response().end("<h1>Hello from my first Vert.x application</h1>");
-	  	})
-	  	.listen(443, result -> {
-	  		if ( result.succeeded() ) {
-	  			fut.complete();
-	  		}
-	  		else {
-	  			fut.fail(result.cause());
-	  		}
-	  	});
-	  	*/
-
-  //}
 }
 
